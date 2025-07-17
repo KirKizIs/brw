cac7a52370688b5097037c1065d67aff68a532d9
KirKizIs <sapetp54@gmail.com>
2025-07-18 01:00:29 +0300

    create and setup spring boot application for api schedule

---
+++ backend/schedule/src/main/java/me/wydentis/BrwSchedule.java
@@ -0,0 +1,13 @@
+package me.wydentis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrwSchedule {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrwSchedule.class, args);
+    }
+
+}
